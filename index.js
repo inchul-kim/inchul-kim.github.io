b677fc2b520e74e78340fd2e549a980877b6ad39
inchul-kim <inchulkim4751@gmail.com>
2023-07-29 00:51:41 +0900

    colorscheme.css, dark mode img swap implemented

--- index.js
+++ index.js
@@ -24,17 +24,43 @@ function topFunction() {
 /**
  * dark mode
  */
-function toggleDarkMode() {
-    document.body.classList.toggle("dark");
+function setPicturesThemed(colorScheme = undefined) {
+	// Clean up all existing picture sources that were cloned
+	document.querySelectorAll('picture > source[data-cloned-theme]').forEach(el => {
+		el.remove();
+	});
+
+	if (colorScheme) {
+		// Find all picture sources with the desired colour scheme
+		document.querySelectorAll(`picture > source[media*="(prefers-color-scheme: ${colorScheme})"]`).forEach(el => {
+			// 1. Clone the given <source>
+			// 2. Remove the media attribute so the new <source> is unconditional
+			// 3. Add a "data-cloned-theme" attribute to it for future reference / removal
+			// 4. Prepend the new <source> to the parent <picture> so it takes precedence
+			const cloned = el.cloneNode();
+			cloned.removeAttribute('media');
+			cloned.setAttribute('data-cloned-theme', colorScheme);
+			el.parentNode.prepend(cloned);
+		});
+	}
+}
+
+function setLightDark() {
     if (document.body.classList[0] !== undefined && document.body.classList[0] === 'dark') {
-        document.getElementById("img_back_to_top").src = "./images/misc/up-arrow-svgrepo-com-tangerine.svg";
         localStorage.setItem("dark-mode", "enabled");
+        setPicturesThemed("dark");
     }
     else {
-        document.getElementById("img_back_to_top").src = "./images/misc/up-arrow-svgrepo-com-steelblue.svg";
         localStorage.setItem("dark-mode", "disabled");
+        setPicturesThemed("light");
     }
 }
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+    setLightDark();
+}
+
 
 // Dark mode toggle listener
 const darkModeToggle = document.getElementById("toggle_darkmode");
@@ -44,6 +70,8 @@ darkModeToggle.addEventListener("change", () => {
 
 // Save the dark mode setting to a local browser.
 if (localStorage.getItem("dark-mode") === "enabled") {
-    // setDarkMode(); // set state of darkMode on page load
-    document.getElementById("toggle_darkmode").click()
+    document.getElementById("toggle_darkmode").click();
+}
+else {
+    setLightDark("light");
 }
