b77b30b0b8be952c0d3cc29a4989be1f07be6fa7
inchul-kim <inchulkim4751@gmail.com>
2023-08-09 16:51:23 +0900

    ongoing

--- back_to_top_btn.js
+++ back_to_top_btn.js
@@ -2,7 +2,7 @@ let xhr = new XMLHttpRequest();
 
 xhr.open("GET", "element_htmls/back_to_top_btn.html", true);
 xhr.send();
-xhr.onload() = () => {
+xhr.onload = () => {
     if (xhr.status == 200) {
         // success
         document.getElementById("main_div").innerHTML += xhr.responseText;
@@ -29,33 +29,3 @@ xhr.onload() = () => {
         }
     }
 }
-
-// $.ajax({
-//     url : "element_htmls/back_to_top_btn.html",
-//     type : "get",
-//     async: false,
-//     success : function(btnHtml) {
-//         document.getElementById("main_div").innerHTML += btnHtml;
-//         /**
-//          * back to top
-//          */
-//         // Get the button
-//         let mybutton = document.getElementById("btn_back_to_top");
-
-//         // When the user scrolls down 20px from the top of the document, show the button
-//         window.onscroll = function () { scrollFunction() };
-
-//         function scrollFunction() {
-//             if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//                 mybutton.style.display = "block";
-//             } else {
-//                 mybutton.style.display = "none";
-//             }
-//         }
-
-//         // When the user clicks on the button, scroll to the top of the document
-//         function topFunction() {
-//             window.scrollTo({ top: 0, behavior: 'smooth' });
-//         }
-//     },
-//  });
