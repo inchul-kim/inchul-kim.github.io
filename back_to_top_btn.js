458f5f244c2cc601031224c86059255eb0b08cfc
inchul-kim <inchulkim4751@gmail.com>
2023-08-09 15:34:04 +0900

    ongoing

--- back_to_top_btn.js
+++ back_to_top_btn.js
@@ -2,15 +2,18 @@ $(document).ready(function()
 {
   $.get("element_htmls/back_to_top_btn.html", function(btnHtml)
    {
-      document.getElementById("main_div").appendChild(btnHtml);
+    document.getElementById("main_div").innerHTML += btnHtml;
    },'html');    // this is the change now its working
 });
 
-fetch("element_htmls/back_to_top_btn.html")
-  .then((res) => res.text())
-  .then((text) => {
-    // do something with "text"
-   })
-  .catch((e) => console.error(e));
-
-
+// const btnHtml = `
+// <!-- back to top button -->
+// <button onclick="topFunction()" id="btn_back_to_top" title="Go to top">
+//   <picture>
+//     <source srcset="./images/misc/up-arrow-svgrepo-com-dark.svg" media="(prefers-color-scheme: dark)">
+//     <source srcset="./images/misc/up-arrow-svgrepo-com-light.svg" media="(prefers-color-scheme: light)">
+//     <img id="img_back_to_top" src="./images/misc/up-arrow-svgrepo-com-light.svg" width="40px" height="40px"/>
+//   </picture>
+// </button>
+// `;
+// document.getElementById("main_div").innerHTML += btnHtml;
