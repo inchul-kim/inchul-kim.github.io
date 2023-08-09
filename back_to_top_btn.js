4959041f386b4f0d31282dba6d73297c9a7c85ca
inchul-kim <inchulkim4751@gmail.com>
2023-08-09 17:33:47 +0900

    ongoing

--- back_to_top_btn.js
+++ back_to_top_btn.js
@@ -1,36 +1,75 @@
-let xhr = new XMLHttpRequest();
+// let xhr = new XMLHttpRequest();
 
-xhr.open("GET", "element_htmls/back_to_top_btn.html", false);
-xhr.send();
-xhr.onload = () => {
-    if (xhr.status == 200) {
-        // success
-        document.getElementById("main_div").innerHTML += xhr.responseText;
-        /**
-         * back to top
-         */
-        // Get the button
-        let mybutton = document.getElementById("btn_back_to_top");
+// xhr.open("GET", "element_htmls/back_to_top_btn.html", false);
+// xhr.send();
+// xhr.onload = () => {
+//     if (xhr.status == 200) {
+//         // success
+//         document.getElementById("main_div").innerHTML += xhr.responseText;
+//         /**
+//          * back to top
+//          */
+//         // Get the button
+//         let mybutton = document.getElementById("btn_back_to_top");
 
-        // When the user scrolls down 20px from the top of the document, show the button
-        window.onscroll = function () { scrollFunction() };
+//         // When the user scrolls down 20px from the top of the document, show the button
+//         window.onscroll = function () { scrollFunction() };
 
-        function scrollFunction() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                mybutton.style.display = "block";
-            } else {
-                mybutton.style.display = "none";
-            }
-        }
+//         function scrollFunction() {
+//             if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//                 mybutton.style.display = "block";
+//             } else {
+//                 mybutton.style.display = "none";
+//             }
+//         }
 
-        // When the user clicks on the button, scroll to the top of the document
-        function topFunction() {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        }
+//         // When the user clicks on the button, scroll to the top of the document
+//         function topFunction() {
+//             window.scrollTo({ top: 0, behavior: 'smooth' });
+//         }
 
-        // button click listener
-        mybutton.addEventListener("click", function() {
-            topFunction();
-        });
+//         // button click listener
+//         mybutton.addEventListener("click", function() {
+//             topFunction();
+//         });
+//     }
+// }
+
+const btnHtml = `
+<!-- back to top button -->
+<button id="btn_back_to_top" title="Go to top">
+  <picture>
+    <source srcset="./images/misc/up-arrow-svgrepo-com-dark.svg" media="(prefers-color-scheme: dark)">
+    <source srcset="./images/misc/up-arrow-svgrepo-com-light.svg" media="(prefers-color-scheme: light)">
+    <img id="img_back_to_top" src="./images/misc/up-arrow-svgrepo-com-light.svg" width="40px" height="40px"/>
+  </picture>
+</button>
+`;
+
+document.getElementById("main_div").innerHTML += btnHtml;
+/**
+ * back to top
+ */
+// Get the button
+let mybutton = document.getElementById("btn_back_to_top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
     }
 }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+// button click listener
+mybutton.addEventListener("click", function() {
+    topFunction();
+});
