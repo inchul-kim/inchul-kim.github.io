baeadfb1e672a69f5cf8177ddcc5dd8cfda5b75e
inchul-kim <inchulkim4751@gmail.com>
2023-08-09 15:11:46 +0900

    back_to_top_btn.js ongoing

--- back_to_top_btn.js
+++ back_to_top_btn.js
@@ -1,21 +1,21 @@
 // btnHtml = '';
 
+let btnHtml = '';
 
 $(document).ready(function()
 {
   $.get("element_htmls/back_to_top_btn.html", function(html_string)
    {
-      console.log(html_string)
+      btnHtml = html_string;
    },'html');    // this is the change now its working
 });
 
-// fetch("element_htmls/back_to_top_btn.html")
-//   .then((res) => res.text())
-//   .then((text) => {
-//     // do something with "text"
-//    })
-//   .catch((e) => console.error(e));
+fetch("element_htmls/back_to_top_btn.html")
+  .then((res) => res.text())
+  .then((text) => {
+    // do something with "text"
+   })
+  .catch((e) => console.error(e));
 
-// let btnHtml = readFile()
 
-// document.getElementById("main_div").appendChild(btnHtml);
+document.getElementById("main_div").appendChild(btnHtml);
